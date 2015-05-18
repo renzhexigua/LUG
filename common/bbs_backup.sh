71897fc174faf18b39c56dee94fe869aaf234475
renzhexigua <renzhexigua@163.com>
2015-05-19 00:12:34 +0800

    add tar's progress bar via pv
    1. you need install pv;
    2. pass data's size via du with bytes level may calculate more
    accurately.(100 percent display).If you use du -sh with M level,
    it maybe round-up when passing size value to pv,but the actual
    size transfered is littel than the value before,it causes the final
    percent displayed may not be 100%.

--- common/bbs_backup.sh
+++ common/bbs_backup.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 date=`date +"%Y%m%d%H%M"`
 
 #Step1. backup mysql
-mysqldump -uUserName DatabaseName > /home/dir/${date}.sql
+echo "Dumping bbs Database ..."
+mysqldump -uUserName DatabaseName> /home/dir/${date}.sql
 
 #Step2. backup file
-tar -zcf /home/dir/${date}.tgz -C /var/www/dir subdir1 subdir2
+echo "Compressing bbs files ..."
+tar -C /var/www/dir -cf - subdir1 subdir2 subdir3| \
+   pv -s $(du -csb /var/www/dir/{subdir1,subdir2,subdir3} | grep total | awk '{print $1}') | \
+      gzip > /home/dir/${date}.tgz
